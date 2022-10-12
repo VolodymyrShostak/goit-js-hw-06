9cbe0b4fe1f6992e9c090bf2f14e6cc379900bfc
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-12 19:52:20 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,4 @@
+const listWithId = document.querySelector("#categories");
+console.log(listWithId);
 const menuItemsByClass = document.querySelectorAll(".menu-item");
 console.log(menuItemsByClass);
