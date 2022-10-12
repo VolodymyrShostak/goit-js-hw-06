3da29cc95ec34b9398076ca07920a1b876b2422f
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-12 23:07:28 +0300

    завдання 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,12 +1,21 @@
 const listWithId = document.querySelector("#categories");
+
 console.log("Number of categories:", listWithId.childElementCount);
-
-// const menuItemsByClass = document.querySelectorAll(".item");
-
-// console.log(menuItemsByClass.length);
-
-// const titleItemsByTag = document.querySelectorAll("h2");
-// console.log(titleItemsByTag);
-
-// const dfgRg = menuItemsByClass.firstChild;
-// console.log(dfgRg);
+console.log("");
+console.log("Category: ", listWithId.children[0].firstElementChild.textContent);
+console.log(
+  "Elements: ",
+  listWithId.children[0].lastElementChild.childElementCount
+);
+console.log("");
+console.log("Category: ", listWithId.children[1].firstElementChild.textContent);
+console.log(
+  "Elements: ",
+  listWithId.children[1].lastElementChild.childElementCount
+);
+console.log("");
+console.log("Category: ", listWithId.children[2].firstElementChild.textContent);
+console.log(
+  "Elements: ",
+  listWithId.children[2].lastElementChild.childElementCount
+);
