4131c9ff334818d2bbcb5bfd83b880e5a576df07
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-13 15:11:14 +0300

    завдання 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const arr = [];
+for (let i = 0; i < ingredients.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = ingredients[i];
+  li.classList.add("item");
+  arr.push(li);
+}
+
+const ul = document.querySelector("ul");
+ul.prepend(...arr);
