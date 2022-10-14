c50e1b43cba9dfe4b0c54856b0741fffeb106841
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-14 23:42:55 +0300

    завдання 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const formRef = document.querySelector(`.login-form`);
+formRef.addEventListener("submit", onFormSubmit);
 
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  if (
+    e.currentTarget.elements.email.value &&
+    e.currentTarget.elements.password.value
+  ) {
+    const mail = e.currentTarget.elements.email.value;
+    const password = e.currentTarget.elements.password.value;
+    const formData = {
+      mail,
+      password,
+    };
+    console.log(formData);
+    formRef.reset();
+  } else {
+    alert` Всі поля повинні бути заповнені.`;
+  }
+}
