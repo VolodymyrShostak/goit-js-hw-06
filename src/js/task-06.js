01737df4bb43a3656e918a7741e44223c9cf3a3e
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-14 21:52:42 +0300

    завдання 6

---
+++ src/js/task-06.js
@@ -0,0 +1,9 @@
+const inField = document.querySelector(`[data-length="6"]`);
+
+inField.addEventListener("blur", (e) => {
+  if (e.currentTarget.value.length == inField.getAttribute("data-length")) {
+    inField.classList.add("valid");
+  } else {
+    inField.classList.add("invalid");
+  }
+});
