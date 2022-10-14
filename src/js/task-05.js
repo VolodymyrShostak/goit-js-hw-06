ae03b4b081c6bf09be8229a94ce58f49c391907e
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-14 19:18:00 +0300

    завдання №5

---
+++ src/js/task-05.js
@@ -0,0 +1,12 @@
+const inField = document.querySelector(`#name-input`);
+const outField = document.querySelector(`#name-output`);
+
+inField.addEventListener("input", (e) => {
+  if (e.target.value !== "") {
+    outField.textContent = e.target.value;
+  } else {
+    outField.textContent = `Anonymous`;
+  }
+});
+
+outField.textContent;
