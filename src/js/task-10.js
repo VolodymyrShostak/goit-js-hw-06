84e2eb7b3b528d8ad693fbf211f71f8b354edab5
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-15 13:26:13 +0300

    10 завдання

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,27 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const inRef = document.querySelector("input");
+const crtRef = document.querySelector("[data-create]");
+const destRef = document.querySelector("[data-destroy]");
+const boxes = document.getElementById("boxes");
+
+inRef.addEventListener("input", (e) => {
+  const amount = e.currentTarget.value;
+
+  crtRef.addEventListener("click", (e) => {
+    let size = 30;
+    for (let i = 0; i <= amount - 1; i++) {
+      let div = document.createElement("div");
+      div.style.width = `${size}px`;
+      div.style.height = `${size}px`;
+      div.style.backgroundColor = getRandomHexColor();
+      boxes.append(div);
+      size += 10;
+      function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+      }
+    }
+  });
+});
+destRef.addEventListener("click", destroyBoxes);
+function destroyBoxes() {
+  boxes.innerHTML = "";
 }
