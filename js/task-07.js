33c83a3be389a62c269dccc07d9ee8513b33ad05
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-18 15:27:55 +0300

    зауваження до №7 враховано

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inField = document.querySelector(`input#font-size-control`);
 let msgRef = document.querySelector(`#text`);
 inField.addEventListener("input", (e) => {
-  let letSize = e.currentTarget.value * 1.5;
+  let letSize = e.currentTarget.value;
   msgRef.style.fontSize = `${letSize}px`;
 });
