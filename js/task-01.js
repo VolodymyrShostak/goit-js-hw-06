b033a0181e69fedd65d74e7ca4793afb0157e771
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-18 22:00:39 +0300

    із виправленням №9

--- js/task-01.js
+++ js/task-01.js
@@ -2,28 +2,8 @@ const categories = document.querySelectorAll(`.item`);
 
 console.log("Number of categories:", categories.length);
 console.log("");
-
 for (const li of Array.from(categories)) {
   console.log("Category: ", li.firstElementChild.textContent);
   console.log("Elements: ", li.lastElementChild.children.length);
   console.log("");
 }
-
-//  for (i = 0; i<)
-// console.log("Category: ", listWithId.children[0].firstElementChild.textContent);
-// console.log(
-//   "Elements: ",
-//   listWithId.children[0].lastElementChild.childElementCount
-// );
-// console.log("");
-// console.log("Category: ", listWithId.children[1].firstElementChild.textContent);
-// console.log(
-//   "Elements: ",
-//   listWithId.children[1].lastElementChild.childElementCount
-// );
-// console.log("");
-// console.log("Category: ", listWithId.children[2].firstElementChild.textContent);
-// console.log(
-//   "Elements: ",
-//   listWithId.children[2].lastElementChild.childElementCount
-// );
--- js/task-09.js
+++ js/task-09.js
@@ -6,10 +6,13 @@ const nmbColor = document.querySelector(`.color`);
 const body = document.querySelector(`body`);
 
 btnChangeColor.addEventListener(`click`, changeColor);
+
 function changeColor(e) {
-  body.style.backgroundColor = getColor();
-  nmbColor.textContent = getColor();
+  const color = getColor();
+  body.style.backgroundColor = color;
+  nmbColor.textContent = color;
 }
+
 const getColor = function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
