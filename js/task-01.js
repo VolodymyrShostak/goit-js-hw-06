c415ec5eae5d5e39346307ddaff60a3bac7c5aa7
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-10-18 15:17:49 +0300

    перероблене завдання №1

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,29 @@
-const listWithId = document.querySelector("#categories");
+const categories = document.querySelectorAll(`.item`);
 
-console.log("Number of categories:", listWithId.childElementCount);
+console.log("Number of categories:", categories.length);
 console.log("");
-console.log("Category: ", listWithId.children[0].firstElementChild.textContent);
-console.log(
-  "Elements: ",
-  listWithId.children[0].lastElementChild.childElementCount
-);
-console.log("");
-console.log("Category: ", listWithId.children[1].firstElementChild.textContent);
-console.log(
-  "Elements: ",
-  listWithId.children[1].lastElementChild.childElementCount
-);
-console.log("");
-console.log("Category: ", listWithId.children[2].firstElementChild.textContent);
-console.log(
-  "Elements: ",
-  listWithId.children[2].lastElementChild.childElementCount
-);
+
+for (const li of Array.from(categories)) {
+  console.log("Category: ", li.firstElementChild.textContent);
+  console.log("Elements: ", li.lastElementChild.children.length);
+  console.log("");
+}
+
+//  for (i = 0; i<)
+// console.log("Category: ", listWithId.children[0].firstElementChild.textContent);
+// console.log(
+//   "Elements: ",
+//   listWithId.children[0].lastElementChild.childElementCount
+// );
+// console.log("");
+// console.log("Category: ", listWithId.children[1].firstElementChild.textContent);
+// console.log(
+//   "Elements: ",
+//   listWithId.children[1].lastElementChild.childElementCount
+// );
+// console.log("");
+// console.log("Category: ", listWithId.children[2].firstElementChild.textContent);
+// console.log(
+//   "Elements: ",
+//   listWithId.children[2].lastElementChild.childElementCount
+// );
